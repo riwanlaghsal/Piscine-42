2db518d73547c1cdfcf4c41be00ddcf5636042b3
Laghsal Riwan <20233810@etud.univ-evry.fr>
2024-10-24 11:42:15 +0000

    ex05

---
+++ C01/ex05/ft_putstr.c
@@ -0,0 +1,17 @@
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_putstr(char *str)
+{
+	int index = 0;
+	
+	while (str[index] != '\0')
+	{
+		ft_putchar(str[index]);
+		index++;
+	}
+}
