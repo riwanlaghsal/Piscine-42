3d0a60c5848f6d7880da3e6493d0e892d8e99fa0
Laghsal Riwan <20233810@etud.univ-evry.fr>
2024-10-25 10:45:44 +0000

    ex05

---
+++ C02/ex05/ft_str_is_uppercase.c
@@ -0,0 +1,17 @@
+int	ft_str_is_uppercase(char *str)
+{
+	if (str[0] == '\0')
+	{
+		return 1;
+	}
+	int i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < 'A' || str[i] > 'Z')
+		{
+			return 0;
+		}
+		i++;
+	}
+	return 1;
+}
