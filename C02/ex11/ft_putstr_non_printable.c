66855374087f408d3ee24a67a593b8dd3059c20f
Laghsal Riwan <20233810@etud.univ-evry.fr>
2024-11-16 12:37:56 +0000

    ex11

---
+++ C02/ex11/ft_putstr_non_printable.c
@@ -0,0 +1,42 @@
+#include <unistd.h>
+
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void print_hex(char c)
+{
+	char *hex = "0123456789abcdef";
+	ft_putchar('\\');
+	ft_putchar(hex[(c >> 4) & 0xF]);
+	ft_putchar(hex[c & 0xF]);
+}
+
+void	ft_putstr_non_printable(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 32 && str[i] <= 126)
+		{
+			ft_putchar(str[i]);
+		}
+		else
+		{
+			print_hex(str[i]);
+		}
+		i++;
+	}
+
+}
+
+/*
+int	main(void)
+{
+	char *str;
+	str = "Coucou\n tu vas bien ?";
+	ft_putstr_non_printable(str);
+	return 0;
+} */
